86b3f526b62f91c9b732253c7265cb9a32acbeb1
solux <loctranphuoc456@protonmail.com>
2024-11-01 16:33:15 +0700

    Documentation improvements

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -5,15 +5,17 @@ import {
   InternalServerErrorException,
   Post,
   Req,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthenticatedGuard } from './guards/authenticated.guard';
 import { User as AuthUser } from '@/shared/decorators/user.decorator';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateUserDto } from '@/users/dtos/CreateUserDto';
 import { User } from '@/users/models/user.model';
-import { LoginDto } from './dtos/LoginDto';
+import { LoginDto } from './dtos/login.dto';
+import { SuccessResponse } from '@/shared/classes/success-response.class';
+import { createResponseType } from '@/shared/helpers/create-response.mixin';
 
 @ApiTags('session')
 @Controller({
@@ -28,36 +30,66 @@ export class SessionController {
   @ApiResponse({
     status: 200,
     description: 'User successfully logged in',
-    type: User,
+    type: createResponseType('User successfully logged in', User),
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Not logged in',
+    type: UnauthorizedException,
+    example: new UnauthorizedException().getResponse(),
   })
   @UseGuards(LocalAuthGuard)
   @Post()
-  @ApiResponse({ status: 401, description: 'Not logged in' })
   async login(@AuthUser() user) {
     const { hashedPassword, ...rest } = user;
-    return rest;
+    return new SuccessResponse('User successfully logged in', rest);
   }
 
   @ApiOperation({ summary: 'Get logged-in user information' })
-  @ApiResponse({ status: 200, description: 'Request successful', type: User })
-  @ApiResponse({ status: 401, description: 'Not logged in' })
+  @ApiResponse({
+    status: 200,
+    description: 'Request successful',
+    type: createResponseType('Session info queried successfully', User),
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Not logged in',
+    type: UnauthorizedException,
+    example: new UnauthorizedException().getResponse(),
+  })
   @UseGuards(AuthenticatedGuard)
   @Get()
   async getLoggedInUser(@AuthUser() user) {
     const { hashedPassword, ...rest } = user;
-    return rest;
+    return new SuccessResponse('Session info queried successfully', rest);
   }
 
   @ApiOperation({ summary: 'Sign out' })
-  @ApiResponse({ status: 200, description: 'Successfully logged out' })
-  @ApiResponse({ status: 401, description: 'Not logged in' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully logged out',
+    type: createResponseType('Successfully logged out', null),
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Not logged in',
+    type: UnauthorizedException,
+    example: new UnauthorizedException().getResponse(),
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+    type: InternalServerErrorException,
+    example: new InternalServerErrorException().getResponse(),
+  })
   @UseGuards(AuthenticatedGuard)
   @Delete()
   async logOut(@Req() req) {
     req.logout((err) => {
       if (err) {
-        throw new InternalServerErrorException();
+        throw new InternalServerErrorException(err);
       }
+      return new SuccessResponse('Successfully logged out', null);
     });
   }
 }
